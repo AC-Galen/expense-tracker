6436bfd5a30fd164fcdc699628b1ce87ffeb66f4
AC-Galen <lottaie7359882@gmail.com>
2022-06-24 10:41:02 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-//載入 express 並建構應用程式
 const express = require("express")
+const mongoose = require('mongoose')
 const app = express()
 
-//設定首頁路由
+//設定連線到MONGODB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//取得資料庫連線狀態
+const db = mongoose.connection
+//連線異常
+db.on('error', (req, res) => {
+  console.log('mongodb error')
+})
+db.once('open', (req, res) => {
+  console.log('mongodb connected')
+})
+
+
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-//設定port 3000
+
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
---
+++ model/category.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  id: { type: Number, required: true },
+  name: { type: String, required: true }
+})
+
+module.exports = mongoose.model('Category', userSchema)
